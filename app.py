d3780ad45c1707d9515a01c79756e3f46dd44c5b
della <rodrigoj@sssistemas.com>
2025-03-13 23:16:36 -0300

    v4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from Sped import Sped
 
-sped = Sped(val_entrada=5054066.96,  val_saida=1216975.58, directory="", file="EFD_0001_Fevereiro_2023.sped", fator=3)
+sped = Sped(val_entrada= 5910294.17,  val_saida=520070.83, directory="", file="EFD_0001_Fevereiro_64435670000113_2025.sped", fator=1)
 sped.processarh010(sped)
